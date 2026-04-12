6d6a39be81d6071fa9c694e109056d3d8840499f
AKFushimijr <alexandrekfushimi@gmail.com>
2026-04-12 19:44:16 -0300

    main

--- src/Processo.java
+++ src/Processo.java
@@ -1,4 +1,68 @@
-package PACKAGE_NAME;
+public class Processo {git --version
+    private int protocolo;
+    private String solicitante;
+    private String tipoDeServico;
+    private int prioridade;
+    private int dataHora;
+    private static int contador = 1;
 
-public class Processo {
+    // Construtor;
+    public Processo(String solicitante, String tipoDeServico, int prioridade) {
+        this.solicitante = solicitante;
+        this.tipoDeServico = tipoDeServico;
+        this.prioridade = prioridade;
+        this.protocolo = contador;
+        contador++;
+    }
+
+    // Getters e Setters
+
+    public int getProtocolo() {
+        return protocolo;
+    }
+
+    public void setProtocolo(int protocolo) {
+        this.protocolo = protocolo;
+    }
+
+    public String getSolicitante() {
+        return solicitante;
+    }
+
+    public void setSolicitante(String solicitante) {
+        this.solicitante = solicitante;
+    }
+
+    public String getTipoDeServico() {
+        return tipoDeServico;
+    }
+
+    public void setTipoDeServico(String tipodeServico) {
+        this.tipoDeServico = tipodeServico;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public int getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(int dataHora) {
+        this.dataHora = dataHora;
+    }
+// Metodos
+
+
+    @Override
+    public String toString(){
+
+
+        return "[#" +  String.format("%03d",this.protocolo) +"] " + this.solicitante + " | " + this.tipoDeServico +" | "+this.prioridade;
+    }
 }
---
+++ src/TesteProcesso.java
@@ -0,0 +1,9 @@
+public class TesteProcesso {
+    public static void main(String[] args) {
+        Processo p1 = new Processo("Alex","Endocrino", 2);
+        Processo p2 = new Processo("Alex","Endocrino", 2);
+
+        System.out.println(p1);
+        System.out.println(p2);
+    }
+}
